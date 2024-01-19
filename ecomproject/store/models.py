60c17c2e073999e121b1a29a10a77287ef890c8c
Rahul Kumar Bhagat <rkb.ucet@gmail.com>
2024-01-19 12:34:53 +0530

    rendering product and cart details dynamically

--- ecomproject/store/models.py
+++ ecomproject/store/models.py
@@ -14,9 +14,18 @@ class Product(models.Model):
     name = models.CharField(max_length=255, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=False)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
     
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
@@ -32,6 +41,14 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.product)
+    
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 
 class ShippingAddress(models.Model):
--- ecomproject/store/views.py
+++ ecomproject/store/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def store(request):
-    context = {}
+    product = Product.objects.all()
+    context = {'product': product}
     return render(request, 'store/Store.html', context)
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+    context = {'items': items}
     return render(request, 'store/Cart.html', context)
 
 def checkout(request):
