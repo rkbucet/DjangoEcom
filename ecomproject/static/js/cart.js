27246d9efa3a34eb46b97edee63522842505f01c
Rahul Kumar Bhagat <rkb.ucet@gmail.com>
2024-01-22 11:29:32 +0530

    clickEvent testing

---
+++ ecomproject/static/js/cart.js
@@ -0,0 +1,14 @@
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for(var i = 0; i< updateBtns.length; i++){
+    updateBtns[i].addEventListener('click', function(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+    console.log('productId:', productId, 'action:', action)
+    console.log(user)
+    if(user === 'AnonymousUser'){
+        console.log('Not loged in!')
+    }else{
+        console.log('Logged in, sending data ...')
+    }
+})}
--- ecomproject/store/views.py
+++ ecomproject/store/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from .models import *
 
@@ -19,5 +20,16 @@ def cart(request):
     return render(request, 'store/Cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context = {'items': items, 'order': order}
     return render(request, 'store/Checkout.html', context)
+
+
+def updateItem(self):
+    return JsonResponse("Item added", safe=False)
